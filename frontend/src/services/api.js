7f71d13e64a1dcccc9b8387507b78f919cf184f9
Snehal Abnave <snehalabnave.sae.comp@gmail.com>
2026-04-22 17:43:25 +0530

    Auth updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const TOKEN_KEY = "govproposal_token";
 
-const API_URL = "http://127.0.0.1:8000";
-
+const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
   baseURL: API_URL,
   headers: {
